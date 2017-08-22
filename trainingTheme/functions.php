4ca7b9df88987bd56042b4ef17df4e0c796f09f2
cyndyalisia <30918137+cyndyalisia@users.noreply.github.com>
2017-08-22 14:51:24 +0700

    Update functions.php

--- trainingTheme/functions.php
+++ trainingTheme/functions.php
@@ -3,16 +3,39 @@
         wp_register_style('bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
         $dependencies = array('bootstrap');
         wp_enqueue_style( 'trainingTheme-style', get_stylesheet_uri(), $dependencies );
+        if ( is_child_theme() ) {
+            // load parent stylesheet first if this is a child theme
+    	       wp_enqueue_style( 'parent-stylesheet', trailingslashit( get_template_directory_uri() ) . 'style.css', false );
+        }
+        // load active theme stylesheet in both cases
+        wp_enqueue_style( 'theme-stylesheet', get_stylesheet_uri(), false );
+
     }
 
     function trainingTheme_enqueue_scripts() {
-        $dependencies = array('jquery');
-        wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '3.3.6', true );
+        if(function_exists('trainingTheme_enqueue_styles')){
+          $dependencies = array('jquery');
+          wp_enqueue_script('bootstrap', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', $dependencies, '3.3.6', true );
+        }
+    }
+
+    function wpb_custom_new_menu() {
+        if(function_exists('trainingTheme_enqueue_scripts')){
+            register_nav_menus(
+              array(
+                'my-custom-menu' => __( 'My Custom Menu' ),
+                'extra-menu' => __( 'Extra Menu' )
+              )
+            );
+        }
     }
 
     function trainingTheme_wp_setup() {
-        add_theme_support( 'title-tag' );
+        if(function_exists('wpb_custom_new_menu')){
+            add_theme_support( 'title-tag' );
+        }
     }
+
     register_sidebar( array(
         'name'          => 'Sidebar - Inset',
         'id'            => 'sidebar-1',
@@ -32,18 +55,9 @@
     ) );
 
 
-    function wpb_custom_new_menu() {
-        register_nav_menus(
-          array(
-            'my-custom-menu' => __( 'My Custom Menu' ),
-            'extra-menu' => __( 'Extra Menu' )
-          )
-        );
-      }
 
-    
-    add_action( 'init', 'wpb_custom_new_menu' );
     add_action( 'wp_enqueue_scripts', 'trainingTheme_enqueue_styles' );
     add_action( 'wp_enqueue_scripts', 'trainingTheme_enqueue_scripts' );
+    add_action( 'init', 'wpb_custom_new_menu' );
     add_action( 'after_setup_theme', 'trainingTheme_wp_setup' );
 ?>
