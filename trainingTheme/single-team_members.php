c432c3d20d74ce59b6e800e94fadf4fca528c15b
cyndyalisia <30918137+cyndyalisia@users.noreply.github.com>
2017-08-30 11:40:36 +0700

    Update single-team_members.php

--- trainingTheme/single-team_members.php
+++ trainingTheme/single-team_members.php
@@ -11,31 +11,21 @@ get_header();
         echo $upload;
 ?></p><br>
 
-<h4 style="text-align: center;">
-    <?php if(get_post_meta( get_the_ID(), 'email', true)!= NULL){
-        echo esc_html( get_post_meta( get_the_ID(), 'email', true ) );
+<?php
+    $posting = array('email', 'phone_number', 'website');
+    for ($i=0; $i < 3; $i++) { ?>
+        <h4 style="text-align: center;">
+            <?php if(get_post_meta( get_the_ID(), $posting[$i], true) != NULL ){
+                echo esc_html( get_post_meta( get_the_ID(), $posting[$i], true ) );
+            }
+            else{
+                echo "Have no Data";
+            }
+            ?>
+        </h4><br>
+    <?php
     }
-    else{
-        echo "Have no Email";
-    } ?>
-</h4><br>
+    ?>
 
-<h4 style="text-align: center;">
-    <?php if (get_post_meta( get_the_ID(), 'phone_number', true ) != NULL ){
-          echo esc_html(get_post_meta( get_the_ID(), 'phone_number', true ));
-      }
-      else{
-          echo "Have no phone number";
-      } ?>
-</h4><br>
-
-<h4 style="text-align: center;">
-    <?php if(get_post_meta( get_the_ID(), 'website', true) != NULL){
-        echo esc_html( get_post_meta( get_the_ID(), 'website', true));
-    }
-    else{
-        echo "Have no Website";
-    } ?>
-</h4><br>
 
 <?php get_footer(); ?>
